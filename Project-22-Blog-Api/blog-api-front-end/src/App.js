c61cb48cc58beb4633174316d2f541063e06a377
gpjgabriel <gpj_gabriel@hotmail.com>
2022-06-13 18:45:52 -0300

    create blog-api-front folder

---
+++ Project-22-Blog-Api/blog-api-front-end/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+import StrangerThings from './components/StrangerThings';
+
+// GPJ
+
+function App() {
+  return (
+    <div className="App">
+      <StrangerThings />
+    </div>
+  );
+}
+
+export default App;
